f79180b44d21e570124dc35498dfd136f15d1ceb
Jared Boese <jgboese@mavs.coloradomesa.edu>
2018-02-08 09:00:02 -0700

    Set up Database Structure

---
+++ app/src/main/java/edu/colostate/jaredboese/treetour/FBDatabase.java
@@ -0,0 +1,38 @@
+package edu.colostate.jaredboese.treetour;
+
+/**
+ * Created by Jared Boese on 2/8/2018.
+ */
+
+public class FBDatabase {
+    public static class TreeInfo{
+
+        public String CommonName;
+        public String ScienceName;
+        public String Zone;
+        public String Height;
+        public String Crown;
+        public String Color;
+        public String FallColor;
+        public String Fruit;
+
+
+        public TreeInfo() {
+
+        }
+        public TreeInfo(String CommonName, String ScienceName, String Zone, String Height,
+                        String Crown, String Color, String FallColor, String Fruit){
+        this.CommonName = CommonName;
+        this.ScienceName = ScienceName;
+        this.Zone = Zone;
+        this.Height = Height;
+        this.Crown = Crown;
+        this.Color = Color;
+        this.FallColor = FallColor;
+        this.Fruit = Fruit;
+            
+        }
+
+    }
+
+}
