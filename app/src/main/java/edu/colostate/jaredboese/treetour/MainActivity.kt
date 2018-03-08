a5cfa3349ee7dddaa1c3ca2716c61529291915f4
mjjohnson322 <32075876+mjjohnson322@users.noreply.github.com>
2018-03-08 10:05:49 -0700

    Google authentication is now working

--- app/src/main/java/edu/colostate/jaredboese/treetour/MainActivity.kt
+++ app/src/main/java/edu/colostate/jaredboese/treetour/MainActivity.kt
@@ -5,15 +5,26 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    internal var mAuthListener: FirebaseAuth.AuthStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+        if (firebaseAuth.currentUser == null) {
+            startActivity(Intent(this, GoogleLogin::class.java))
+        }
+    }
+    internal var mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onStart() {
+        super.onStart()
+        mAuth.addAuthStateListener(mAuthListener)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var NewBtn = findViewById<Button>(R.id.AButton)
-
-        NewBtn.setOnClickListener {
+        var dataBtn = findViewById<Button>(R.id.DataButton)
+        dataBtn.setOnClickListener {
             val intent = Intent(this,ViewData::class.java)
             startActivity(intent)
         }
